8d2b7036360ded16ecc2b0b0a1ea55f151b1616e
fvn70 <fvn_reg@mail.ru>
2022-03-03 22:08:08 +0700

    Stage 3/5: Endless game

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -2,13 +2,21 @@ import random
 
 things = ['rock', 'paper', 'scissors']
 
-thing = input()
-i = things.index(thing)
-j = random.randint(0, 2)
-choice = things[j]
-if i == j:
-    print(f"There is a draw ({choice})")
-elif j == (i + 1) % 3:
-    print(f"Sorry, but the computer chose {choice}")
-else:
-    print(f"Well done. The computer chose {choice} and failed")
+while True:
+    thing = input()
+    if thing == '!exit':
+        print("Bye!")
+        break
+    if thing not in things:
+        print("Invalid input")
+        continue
+
+    i = things.index(thing)
+    j = random.randint(0, 2)
+    choice = things[j]
+    if i == j:
+        print(f"There is a draw ({choice})")
+    elif j == (i + 1) % 3:
+        print(f"Sorry, but the computer chose {choice}")
+    else:
+        print(f"Well done. The computer chose {choice} and failed")
