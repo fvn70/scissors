b01a3a34285a4e53f74d4b96ec81c9c90a06eb17
fvn70 <fvn_reg@mail.ru>
2022-03-03 21:56:06 +0700

    Stage 2/5: Equalizing chances

--- Rock-Paper-Scissors/task/game.py
+++ Rock-Paper-Scissors/task/game.py
@@ -1,6 +1,14 @@
+import random
+
 things = ['rock', 'paper', 'scissors']
 
 thing = input()
-choice = things[(things.index(thing) + 1) % 3]
-print(f'Sorry, but the computer chose {choice}')
-
+i = things.index(thing)
+j = random.randint(0, 2)
+choice = things[j]
+if i == j:
+    print(f"There is a draw ({choice})")
+elif j == (i + 1) % 3:
+    print(f"Sorry, but the computer chose {choice}")
+else:
+    print(f"Well done. The computer chose {choice} and failed")
